57779ff6b4a8fb89b309283d90e8aaf408cdacdc
Ashish Shah <eiepixxstuff@gmail.com>
2025-03-16 18:45:32 +0530

    version 1.0.0

---
+++ src/constants/index.js
@@ -0,0 +1,248 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    tailwind,
+    mongodb,
+    git,
+    figma,
+    meta,
+    starbucks,
+    tesla,
+    shopify,
+    carrent,
+    jobit,
+    tripguide,
+} from '../assets';
+
+export const navLinks = [
+    {
+        id: 'about',
+        title: 'About',
+    },
+    {
+        id: 'work',
+        title: 'Work',
+    },
+    {
+        id: 'contact',
+        title: 'Contact',
+    },
+];
+
+const services = [
+    {
+        title: 'JavaScript Developer',
+        icon: web,
+    },
+    {
+        title: 'React Developer',
+        icon: mobile,
+    },
+    {
+        title: 'Learner',
+        icon: backend,
+    },
+    {
+        title: 'Tech Article Writer',
+        icon: creator,
+    },
+];
+
+const technologies = [
+    {
+        name: 'HTML 5',
+        icon: html,
+    },
+    {
+        name: 'CSS 3',
+        icon: css,
+    },
+    {
+        name: 'JavaScript',
+        icon: javascript,
+    },
+    {
+        name: 'TypeScript',
+        icon: typescript,
+    },
+    {
+        name: 'React JS',
+        icon: reactjs,
+    },
+
+    {
+        name: 'Tailwind CSS',
+        icon: tailwind,
+    },
+
+    {
+        name: 'MongoDB',
+        icon: mongodb,
+    },
+
+    {
+        name: 'git',
+        icon: git,
+    },
+    {
+        name: 'figma',
+        icon: figma,
+    },
+];
+
+const experiences = [
+    {
+        title: 'React.js Developer',
+        company_name: 'Starbucks',
+        icon: starbucks,
+        iconBg: '#383E56',
+        date: 'March 2020 - April 2021',
+        points: [
+            'Developing and maintaining web applications using React.js and other related technologies.',
+            'Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
+            'Implementing responsive design and ensuring cross-browser compatibility.',
+            'Participating in code reviews and providing constructive feedback to other developers.',
+        ],
+    },
+    {
+        title: 'React Native Developer',
+        company_name: 'Tesla',
+        icon: tesla,
+        iconBg: '#E6DEDD',
+        date: 'Jan 2021 - Feb 2022',
+        points: [
+            'Developing and maintaining web applications using React.js and other related technologies.',
+            'Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
+            'Implementing responsive design and ensuring cross-browser compatibility.',
+            'Participating in code reviews and providing constructive feedback to other developers.',
+        ],
+    },
+    {
+        title: 'Web Developer',
+        company_name: 'Shopify',
+        icon: shopify,
+        iconBg: '#383E56',
+        date: 'Jan 2022 - Jan 2023',
+        points: [
+            'Developing and maintaining web applications using React.js and other related technologies.',
+            'Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
+            'Implementing responsive design and ensuring cross-browser compatibility.',
+            'Participating in code reviews and providing constructive feedback to other developers.',
+        ],
+    },
+    {
+        title: 'Full stack Developer',
+        company_name: 'Meta',
+        icon: meta,
+        iconBg: '#E6DEDD',
+        date: 'Jan 2023 - Present',
+        points: [
+            'Developing and maintaining web applications using React.js and other related technologies.',
+            'Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
+            'Implementing responsive design and ensuring cross-browser compatibility.',
+            'Participating in code reviews and providing constructive feedback to other developers.',
+        ],
+    },
+];
+
+const testimonials = [
+    {
+        testimonial:
+            'I thought it was impossible to make a website as beautiful as our product, but Ashish proved me wrong.',
+        name: 'Sara Lee',
+        designation: 'CFO',
+        company: 'Acme Co',
+        image: 'https://randomuser.me/api/portraits/women/4.jpg',
+    },
+    {
+        testimonial:
+            "I've never met a web developer who truly cares about their clients' success like Ashish does.",
+        name: 'Chris Brown',
+        designation: 'COO',
+        company: 'DEF Corp',
+        image: 'https://randomuser.me/api/portraits/men/5.jpg',
+    },
+    {
+        testimonial:
+            "After Ashish optimized our website, our traffic increased by 50%. We can't thank them enough!",
+        name: 'Lisa Wang',
+        designation: 'CTO',
+        company: '456 Enterprises',
+        image: 'https://randomuser.me/api/portraits/women/6.jpg',
+    },
+];
+
+const projects = [
+    {
+        name: 'Task Flow',
+        description:
+            'A productivity web app that helps users create, manage, and track their daily tasks with an intuitive and interactive interface.',
+        tags: [
+            {
+                name: 'react',
+                color: 'blue-text-gradient',
+            },
+            {
+                name: 'mongodb',
+                color: 'green-text-gradient',
+            },
+            {
+                name: 'tailwind',
+                color: 'pink-text-gradient',
+            },
+        ],
+        image: carrent,
+        source_code_link: 'https://github.com/',
+    },
+    {
+        name: 'Career Connect',
+        description:
+            'A job search platform that allows users to find job listings, estimate salary ranges, and explore opportunities based on location and preferences.',
+        tags: [
+            {
+                name: 'react',
+                color: 'blue-text-gradient',
+            },
+            {
+                name: 'restapi',
+                color: 'green-text-gradient',
+            },
+            {
+                name: 'scss',
+                color: 'pink-text-gradient',
+            },
+        ],
+        image: jobit,
+        source_code_link: 'https://github.com/',
+    },
+    {
+        name: 'Wanderlust',
+        description:
+            'A travel planning application that enables users to explore destinations, book accommodations, and access curated travel guides for an enhanced experience.',
+        tags: [
+            {
+                name: 'nextjs',
+                color: 'blue-text-gradient',
+            },
+            {
+                name: 'supabase',
+                color: 'green-text-gradient',
+            },
+            {
+                name: 'css',
+                color: 'pink-text-gradient',
+            },
+        ],
+        image: tripguide,
+        source_code_link: 'https://github.com/',
+    },
+];
+
+export { services, technologies, experiences, testimonials, projects };
